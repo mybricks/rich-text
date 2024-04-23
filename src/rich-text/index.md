7f4b797f0c05c65106c1662cdd505483a5ea8cdc
wudi27 <wudi27@kuaishou.com>
2024-04-23 16:37:26 +0800

    update

--- src/rich-text/index.md
+++ src/rich-text/index.md
@@ -5,5 +5,21 @@ This is an example component.
 ```jsx
 import { MybricksRichText } from '@mybricks/rich-text';
 
-export default () => <MybricksRichText title="Hello dumi!" />
+export default () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        gap: 8,
+      }}
+    >
+      <MybricksRichText
+        style={{ width: 840, height: 400 }}
+        placeholder="请输入..."
+      />
+      <MybricksRichText style={{ width: 840, height: 400 }} readonly />
+    </div>
+  );
+};
 ```
--- src/rich-text/index.tsx
+++ src/rich-text/index.tsx
@@ -1,22 +1,42 @@
-import React, { useEffect, useMemo, useState, type FC } from 'react';
+import React, { useEffect, useRef, useState, type FC } from 'react';
 import Quill from 'quill';
-import { uuid } from './utils';
 import "quill/dist/quill.core.css";
 import "quill/dist/quill.snow.css";
 import "./index.less";
 
-const MybricksRichText: FC<{ title: string }> = (props) => {
-  const [quill, setQuill] = useState<Quill | null>();
-  const editorId = useMemo(() => `editor-${uuid()}`, []);
+export type MybricksRichTextProps = {
+  value?: string;
+  readonly?: boolean;
+  style?: React.CSSProperties
 
+  onChange?: (value: string) => void;
+}
+
+const MybricksRichText: FC<MybricksRichTextProps> = (props) => {
+  const { value, onChange, readonly, style } = props;
+
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const [editorHeight, setEditorHeight] = useState<number | undefined>(void 0);
+
+  /**
+   * 创建 Quill 富文本编辑器
+   */
   useEffect(() => {
-    setQuill(new Quill(`#${editorId}`, {
+    const editor = wrapperRef.current!.querySelector('div.rich-text-editor') as HTMLElement;
+    const quill = new Quill(editor, {
       modules: {
-        imageUpload: true,
         toolbar: [
-          [{ 'font': [] }, { 'header': [1, 2, 3, 4, 5, 6, false] }, { 'size': ['small', false, 'large', 'huge'] }],
+          [
+            { 'font': [] },
+            { 'header': [1, 2, 3, 4, 5, 6, false] },
+            { 'size': ['small', false, 'large', 'huge'] }
+          ],
           ['bold', 'italic', 'underline', 'strike'],
-          [{ 'align': [] }, { 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
+          [
+            { 'align': [] },
+            { 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }
+          ],
           [{ 'color': [] }, { 'background': [] }],
           ['link', 'image', 'video'],
           ['clean']
@@ -24,12 +44,18 @@ const MybricksRichText: FC<{ title: string }> = (props) => {
       },
       placeholder: '请输入内容...',
       theme: 'snow'
-    }));
-  }, [])
+    });
+
+    quill.enable(!readonly)
+
+    const toolbar = wrapperRef.current!.querySelector('div.ql-toolbar') as HTMLElement;
+    if (wrapperRef.current && toolbar) { setEditorHeight(wrapperRef.current.offsetHeight - toolbar?.offsetHeight); }
+  }, []);
+  
 
   return (
-    <div className='mybricks-rich-text-editor'>
-      <div id={editorId} />
+    <div ref={wrapperRef} className='mybricks-rich-text-editor' style={style}>
+      <div className='rich-text-editor' style={{ height: editorHeight }} />
     </div>
   );
 }
